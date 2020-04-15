2768c14be780910e251ce9a9975a3c86573db81f
AgikFendiHarvyandha <54794520+AgikFendiHarvyandha@users.noreply.github.com>
2020-04-15 16:22:07 +0700

    done
    minggu 11

---
+++ agikfendi-minggu11/src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyDNsR0FmYlVZmxJY9mJngg3YnhP7OI_JJE",
+    authDomain: "webreact-1c511.firebaseapp.com",
+    databaseURL: "https://webreact-1c511.firebaseio.com",
+    projectId: "webreact-1c511",
+    storageBucket: "webreact-1c511.appspot.com",
+    messagingSenderId: "818995422673",
+    appId: "1:818995422673:web:8f3c6043077518a5cf6cf0",
+    measurementId: "G-E7WF8FVQ4M"
+  };
