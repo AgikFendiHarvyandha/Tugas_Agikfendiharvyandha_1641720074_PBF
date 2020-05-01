3e1db7eb161b1ab42b94c1caa5a0047d483b296d
AgikFendiHarvyandha <54794520+AgikFendiHarvyandha@users.noreply.github.com>
2020-05-01 23:03:28 +0700

    praktikum minggu13
    minggu13

---
+++ agikfendi-minggu13/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyADH1GssF5ZvHfH8DZbXuzByuN4Q8KOkD4",
+    authDomain: "login-1b391.firebaseapp.com",
+    databaseURL: "https://login-1b391.firebaseio.com",
+    projectId: "login-1b391",
+    storageBucket: "login-1b391.appspot.com",
+    messagingSenderId: "171818632378",
+    appId: "1:171818632378:web:93d24ed51b81a91067ee83",
+    measurementId: "G-NGMVN26BLV"
+  };
+
+  export default firebaseConfig;
